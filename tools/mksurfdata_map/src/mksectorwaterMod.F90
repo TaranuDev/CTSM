4895e45eb46c52d0c05cef73bf38dd73ad5c8808
Sabin Taranu <sabin.taranu@vub.be>
2022-04-26 09:35:08 +0200

    test

--- tools/mksurfdata_map/src/mksectorwaterMod.F90
+++ tools/mksurfdata_map/src/mksectorwaterMod.F90
@@ -15,7 +15,8 @@ module mksectorwaterMod
   use shr_kind_mod, only : r8 => shr_kind_r8
   use shr_sys_mod , only : shr_sys_flush
   use mkdomainMod , only : domain_checksame
-  use mkvarctl    
+  use mkvarctl   
+  
 
   implicit none
 
@@ -24,6 +25,45 @@ module mksectorwaterMod
   public  :: mksectorwater
 
 contains
+
+subroutine writenetcdffile(array)
+    use netcdf
+    implicit none
+    real, intent(IN), dimension(:) :: array
+
+    integer :: file_id, xdim_id
+    integer :: array_id
+    integer, dimension(1) :: arrdims
+    character(len=*), parameter :: arrunit = 'ergs'
+
+    integer :: i
+    integer :: ierr
+
+    i = size(array,1)
+
+    ! create the file
+    ierr = nf90_create(path='/glade/u/home/staranu/CTSM_input_test/tools/mksurfdata_map/test_array1.nc', cmode=NF90_CLOBBER, ncid=file_id)
+
+    ! define the dimensions
+    ierr = nf90_def_dim(file_id, 'X', i, xdim_id)
+
+    ! now that the dimensions are defined, we can define variables on them,...
+    arrdims = (/ xdim_id /)
+    ierr = nf90_def_var(file_id, 'Array',  NF90_REAL, arrdims, array_id)
+
+    ! ...and assign units to them as an attribute 
+    ierr = nf90_put_att(file_id, array_id, "units", arrunit)
+
+    ! done defining
+    ierr = nf90_enddef(file_id)
+
+    ! Write out the values
+    ierr = nf90_put_var(file_id, array_id, array)
+
+    ! close; done
+    ierr = nf90_close(file_id)
+return
+end subroutine writenetcdffile
 
 !-----------------------------------------------------------------------
 !BOP
@@ -255,6 +295,7 @@ subroutine mksectorwater(ldomain, mapfname, datfname, ndiag, ncido)
      call check_ret(nf_inq_varid (ncidi, 'withd_dom', varid), subname)
      call check_ret(nf_get_vara_double (ncidi, varid, begi(1:ndimsi), leni(1:ndimsi), &
           mdom_withd_i), subname)
+     call writenetcdffile(mdom_withd_i)
      !PRINT *, 'nf_get_vara_double is working for withd_dom'
 
      call check_ret(nf_inq_varid (ncidi, 'cons_dom', varid), subname)
